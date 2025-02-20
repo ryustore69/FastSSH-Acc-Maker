45a29aafbca7d71feb6aa4c0e789d4b63f5abf49
ryustore69 <ryustore69@gmail.com>
2025-02-21 04:49:06 +0700

    test v1.6.6.4

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     async function sendRequest(requestData) {
         try {
-            const proxyUrl = "https://corsproxy.io/?";
+            const proxyUrl = "https://api.allorigins.win/raw?url=";
             const targetUrl = "https://www.fastssh.com/page/create-obfs-process";
             
             const formData = new FormData();
