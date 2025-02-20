bca6094a44a778ac53da9ce05053be37c7d1ff0e
ryustore69 <ryustore69@gmail.com>
2025-02-21 05:15:14 +0700

    test v1.6.6.4.1

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     async function sendRequest(requestData) {
         try {
-            const proxyUrl = "https://sparkling-limit-b5ca.corspass.workers.dev/url=";
+            const proxyUrl = "https://api.allorigins.win/raw?url=";
             const targetUrl = "https://www.fastssh.com/page/create-obfs-process";
             
             const formData = new FormData();
