8af68c98100a897ec6c5ebf527fa9d6d42419813
ryustore69 <ryustore69@gmail.com>
2025-02-21 04:52:03 +0700

    test v.1.6.6.4

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     async function sendRequest(requestData) {
         try {
-            const proxyUrl = "https://api.allorigins.win/raw?url=";
+            const proxyUrl = "https://api.codetabs.com/v1/proxy/?quest=";
             const targetUrl = "https://www.fastssh.com/page/create-obfs-process";
             
             const formData = new FormData();
